793d5ee1c2f546e085daeb47d51a361af65643f8
Skyler recknell <Skyler@Skylers-MacBook-Pro.local>
2017-11-18 14:46:28 -0800

    initial + gitignore

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index.html', { title: 'Cloudant Boiler Plate' });
+};
+
+exports.foo = function(req, res){
+  res.render('foo.html', { title: 'Cloudant Boiler Plate' });
+};
